1899a90c192b4dcab53817126aa9c8eeec16c937
amcgl <mcglynnaj@vcu.edu>
2025-10-21 17:33:10 -0400

    setting up

--- SuperScreen.java
+++ SuperScreen.java
@@ -1,3 +1,14 @@
-public class SuperScreen {
+import javax.swing.*;
 
+
+public abstract class SuperScreen {
+    protected JFrame window;
+
+    public SuperScreen(JFrame window) {
+        this.window = window;
+    }
+
+    public abstract void onEnter();
+    public abstract void onExite();
+    public abstract JPanel getPanel();
 }
